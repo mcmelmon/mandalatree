50dad1b9f7b47266da376cc825456f27d55b6189
mcmelmon <pairs+sunflower@geostellar.com>
2021-09-17 10:33:46 -0700

    Elements, entries, and lists

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, FlatList, SafeAreaView, ScrollView, StyleSheet, Text,TextInput, View } from 'react-native';
 
 export default function App() {
+  const [star, setStar] = useState('');
+  const [stars, setStars] = useState([]);
+
+  const handleStarText = (name) => {
+    setStar(name);
+  }
+
+  const handleAddStar = () => {
+    if (!stars.map(e => e.value).includes(star)) {
+      setStars(currentStars => [...currentStars, { key: stars.length, value: star }]);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.enterStar}>
+        <TextInput 
+          style={styles.input}
+          placeholder="Star"
+          onChangeText={handleStarText}
+          value={star}
+          />
+        <Button 
+          title = "Add"
+          style={styles.btn}
+          onPress={handleAddStar} />
+      </View>
+
+      <FlatList
+        data = {stars}
+        renderItem = {itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>)}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  btn: {
+    flex: 1
   },
+  container: {
+    backgroundColor: '#fff',
+    flex: 1,
+    margin: 20
+  },
+  enterStar: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 20
+  },
+  input: {
+    flex: 1,
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+  },
+  listItem: {
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 10
+  }
 });
